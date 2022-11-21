7ba1b71b54a0b5347355aea9e991a09ef0ed5c0f
bobraozal86 <Bobraozal86@gmail.com>
2022-11-21 13:47:09 +0700

    client frontend

---
+++ wwwroot/js/Department.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+
+    $('#tableDepartment').DataTable({
+        ajax: {
+            url: 'https://localhost:7104/api/Department',
+            dataSrc: 'data',
+            "headers": {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            "type": "GET",
+        },
+        columns: [
+            {
+                data: 'id',
+            },
+            {
+                data: 'name',
+            },
+            {
+                data: null,
+                className: "d-none d-sm-table-cell",
+                "render": function (data, type, row, meta) {
+                    return `<button type="button">Edit</button>
+                        <button type="button">Hapus</button>`
+                }
+            }
+
+        ]
+
+    });
+});
+//$(document).ready(function () {
+//    $("#tableDepartment").DataTable({
+//        $.ajax({
+//            url: "https://localhost:7104/api/Department",
+//            dataSrc: "data",
+//            "type": "GET",
+//        }, columns: [
+//            {
+//                data: 'Id'
+//            },
+//            {
+//                data: 'Name'
+//            },
+//            {
+//                data: null,
+//                "render": function (data, type, row, meta) {
+//                    return
+//                    `<button>Edit</button> <button>Delete</button>`
+//                }
+//            }
+//        ]);
+//    });
+//});
+
